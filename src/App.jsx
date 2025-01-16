0bee73b6479f9b0d0dc0c8583f74e94e3eecd97b
SarvarbekNematov <meoff1011@gmail.com>
2025-01-16 19:28:07 +0500

    fefe

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { Route, Routes } from 'react-router-dom'
-import Tashkent from './page/Tashkent'
-import NewYork from './page/NewYork'
-import London from './page/London'
-import Header from './components/header'
+
 
 function App() {
 
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Tashkent/>}/>
-      <Route path='/newYork' element={<NewYork/>}/> 
-      <Route path='/london' element={<London/>}/> 
-    </Routes>
+    
     </>
   )
 }
